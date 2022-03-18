0c08c2d147c11c8c2cbe0ad4824ac6683d2f170c
Rahul <rahulkotr1@gmail.com>
2022-03-18 20:57:38 +0530

    Add icons

---
+++ app.js
@@ -0,0 +1,4 @@
+const sections =document.querySelectorAll('.section');
+const sectBtns= document.querySelector('.controls');
+const sectBtn=document.querySelector('.control');
+const allSections=document.querySelector('main-content');
