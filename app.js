39e1d541ca51f46680f93dc4c7893cb90eb29257
Rahul <rahulkotr1@gmail.com>
2022-03-18 21:40:59 +0530

    Developing JS

--- app.js
+++ app.js
@@ -2,3 +2,15 @@ const sections =document.querySelectorAll('.section');
 const sectBtns= document.querySelector('.controls');
 const sectBtn=document.querySelector('.control');
 const allSections=document.querySelector('main-content');
+
+function PageTransitions(){
+    for (let i=0; i< sectBtn.length;i++){
+        sectBtn[i].addEventListener('click',function(){
+            let currentBtn=document.querySelectorAll('active-btn');
+            currentBtn[0].className=currentBtn[0].className.replace('active-btn','');
+            this.className+='active-btn';
+        })
+    }
+}
+
+PageTransitions();
