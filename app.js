a9bdc359e5d371f78d38c66dc98d7ad5dba217d9
Rahul <rahulkotr1@gmail.com>
2022-03-18 22:14:16 +0530

    switching pages

--- app.js
+++ app.js
@@ -11,8 +11,21 @@ function PageTransitions(){
             this.className +=' active-btn';
         })
     }
-    allSections.addEventListener('click', (e)=>{
-        const id=e.target.dataset.id;
+    allSections.addEventListener('click', (e) =>{
+        const id = e.target.dataset.id;
+        if(id){
+            sectBtns.forEach((btn) =>{
+                btn.classList.remove('active')
+            })
+            e.target.classList.add('active')
+
+            sections.forEach((section) =>{
+                section.classList.remove('active')
+            })
+
+            const element = document.getElementById(id);
+            element.classList.add('active');
+        }
     })
 
 }
